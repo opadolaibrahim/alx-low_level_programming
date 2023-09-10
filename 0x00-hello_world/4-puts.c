0bfd653f96c569d75e06af2769302ba2e181ef81
Opadolaibrahim <Opadola29@gmail.com>
2023-09-10 04:41:20 -0700

    a C program that prints exactly "Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 /*
- * File: 4-puts.c
- * Auth: Brennan D Baraban <375@holbertonschool.com>
+ * Main - a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line
+ * Return: 0 (Success)
  */
 
 #include <stdio.h>
