fc7aa1b5ba4c6b3e32933d540f75759033f060e0
Opadolaibrahim <Opadola29@gmail.com>
2023-09-10 07:32:28 -0700

    completed task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**
+* main - program that prints with proper grammer,but the outcome is
+*  a piece of art,followed by a new line
+* return (0) success
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
