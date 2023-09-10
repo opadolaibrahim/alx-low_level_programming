371cf61e73a7ebf772fb7c3a1d2049930872f306
Opadolaibrahim <Opadola29@gmail.com>
2023-09-10 09:25:36 -0700

    Advanced task 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a new line
+ * Return: 1 not the usual
+ */
+
+#include<stdio.h>
+int main(void)
+{
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+	return (1);
+}
