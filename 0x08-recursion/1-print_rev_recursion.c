b0817abf350f74035bb441e862c8ca30b94fe7a3
Opadolaibrahim <Opadola29@gmail.com>
2023-09-28 08:00:24 -0700

    Task completed

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,13 @@
 #include "main.h"
 
+#include "holberton.h"
+
 /**
  * _puts_recursion - Prints a string, followed by a new line.
  * @s: The string to be printed.
+ *
+ * Description: This function recursively prints a string, character by
+ * character, followed by a newline character.
  */
 void _puts_recursion(char *s)
 {
@@ -11,7 +16,9 @@ void _puts_recursion(char *s)
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-
 	else
+	{
 		_putchar('\n');
+	}
 }
+
