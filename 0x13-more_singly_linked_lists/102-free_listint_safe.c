7bd2aaab1aa7fc9bee7ba70abf217653ab697c11
Opadola ibrahim lekan <101353575+opadolaibrahim@users.noreply.github.com>
2023-10-25 01:58:09 +0100

    Advanced task redo

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,21 +8,33 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-    size_t len = 0;
-    listint_t *current = *h;
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
 
-    if (!h || !*h)
-        return (0);
+	if (!h || !*h)
+		return (0);
 
-    while (current)
-    {
-        listint_t *temp = current->next;
-        free(current);
-        current = temp;
-        len++;
-    }
+	while (*h)
+	{
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			len++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
+		}
+	}
 
-    *h = NULL;
+	*h = NULL;
 
-    return (len);
+	return (len);
 }
