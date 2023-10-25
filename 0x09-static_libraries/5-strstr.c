24a215e52ebb32f0831ddbb3dd321df05c3a63b4
Opadola ibrahim lekan <101353575+opadolaibrahim@users.noreply.github.com>
2023-10-25 12:15:16 +0100

    Task completed

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strstr - finds the first occurrence of the substr
+ * @haystack: str input 
+ * @needle: substr to match
+ * Return: pointer to the 1st match or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, match;
+
+	if (*needle == '\0')
+		return (haystack);
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == *needle)
+		{
+			for (j = 1; needle[j] != '\0'; j++)
+			{
+				if (needle[j] != haystack[i + j])
+				{
+					match = 0;
+					break;
+				}
+				match = 1;
+			}
+			if (match)
+				return (haystack + i);
+		}
+	}
+	return (NULL);
+}
