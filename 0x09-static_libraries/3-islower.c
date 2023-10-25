c4bb4777f66c943cf869e223660b44d25957f5da
Opadola ibrahim lekan <101353575+opadolaibrahim@users.noreply.github.com>
2023-10-25 11:12:06 +0100

    Task completed

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _islower - function of a line
+ *@c: contains value to be che#include "main.h"
+ *Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+}cked 
+ *Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+}
