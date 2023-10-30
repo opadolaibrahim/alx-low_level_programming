d0b9421135c5b860af940d36bfda384b66f6e6dd
Opadola ibrahim lekan <101353575+opadolaibrahim@users.noreply.github.com>
2023-10-30 22:24:22 +0100

    Task completed

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd_r, fd_w, a, b, i, j;
+	int fd_r, fd_w, x, m, n;
 	char buf[BUFSIZ];
 
 	if (argc != 3)
@@ -23,27 +23,27 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((a = read(fd_r, buf, BUFSIZ)) > 0)
+	while ((x = read(fd_r, buf, BUFSIZ)) > 0)
 	{
-		if (fd_w < 0 || write(fd_w, buf, a) != a)
+		if (fd_w < 0 || write(fd_w, buf, x) != x)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_r);
 			exit(99);
 		}
 	}
-	if (a < 0)
+	if (x < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	i = close(fd_r);
-	j = close(fd_w);
-	if (i < 0 || j < 0)
+	m = close(fd_r);
+	n = close(fd_w);
+	if (m < 0 || n < 0)
 	{
-		if (i < 0)
+		if (m < 0)
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
-		if (j < 0)
+		if (n < 0)
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
 		exit(100);
 	}
